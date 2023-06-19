442cb03bca3e23c1412299f6ab48f4e4ace07cbf
Lucas Grillos <LucasGrillos@gmail.com>
2023-06-19 11:53:00 -0700

    add o-mark, and mark elements to each tile
    add display controller module pattern

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ const player = (m, n) => {
     let getMark = () => mark;
     let getName = () => name;
 
-    return {
-        getMark,
-        getName
-    }
-}
+    return { getMark, getName };
+};
 
 const Gameboard = (() => {
     let gameArray = Array(9).fill('');
-
     const getBoard = () => gameArray;
 
-    return {
-        getBoard
-    }
+    return { getBoard };
+})();
+
+
+const Gameplay = (() => {
+    let player1 = player('x', 'Player 1');
+    let player2 = player('o', 'Player 2');
+    let currentPlayer = player1;
+
+})();
+
+const displayController = (() => {
+
 })();
