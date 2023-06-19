828dd40c890e695a96fdb51c59d6f6d9930b121b
Lucas Grillos <LucasGrillos@gmail.com>
2023-06-19 11:25:16 -0700

    add x mark styles

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const player = (m, n) => {
+    let mark = m;
+    let name = n;
+    let getMark = () => mark;
+    let getName = () => name;
+
+    return {
+        getMark,
+        getName
+    }
+}
+
 const Gameboard = (() => {
     let gameArray = Array(9).fill('');
 
-    const getBoard = () => {
-        return gameArray;
-    }
+    const getBoard = () => gameArray;
 
     return {
         getBoard
