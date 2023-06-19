72801fbd996b373d58c96a2b937e1a2f91d2bb7e
Lucas Grillos <LucasGrillos@gmail.com>
2023-06-19 13:30:33 -0700

    add playerturn function that can add marks to board array, and switches currentplayer

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const player = (m, n) => {
     let mark = m;
     let name = n;
-    let getMark = () => mark;
-    let getName = () => name;
+
+    const getMark = () => mark;
+    const getName = () => name;
 
     return { getMark, getName };
 };
 
 const Gameboard = (() => {
     let gameArray = Array(9).fill('');
+    
     const getBoard = () => gameArray;
+    const markBoard = (tile, mark) => {
+        gameArray[tile] = mark;
+    }
 
-    return { getBoard };
+    return { getBoard, markBoard };
 })();
 
 
@@ -20,14 +25,36 @@ const Gameplay = (() => {
     let player2 = player('o', 'Player 2');
     let currentPlayer = player1;
 
+    const switchPlayers = () => {
+        currentPlayer = (currentPlayer.getName() == player1.getName() ? player2 : player1)
+    };
+
+    const playerTurn = (tile) => {
+        let board = Gameboard.getBoard()
+        if (!board[tile]) {
+            Gameboard.markBoard(tile, currentPlayer.getMark());
+        }
+        switchPlayers();
+        DisplayController.renderBoard();
+        
+    };
+
+    return { playerTurn };
 })();
 
 const DisplayController = (() => {
+
+    let tiles = Array.from(document.querySelector('.gameboard').children);
 
     const makeVisible = (mark, index) => {
         document.querySelector(`#tile${index}`).querySelector(`.${mark}-mark`).style.visibility = 'visible';
         // finds the tile based on index, then finds the mark to fill based on mark passed, then sets visibility to 0
     }
+
+    const tileListener = (event) => {
+        Gameplay.playerTurn(event.target.dataset.tile) 
+        // dataset.tile points to the data-tile attribute
+    };
 
     const renderBoard = () => {
         let board = Gameboard.getBoard();
@@ -38,9 +65,17 @@ const DisplayController = (() => {
         }
     };
 
-    const addTileListeners = () => {
-        console.log(document.querySelector('.gameboard').children);
+    const removeTileListeners = () => {
+        tiles.forEach(tile => {
+            tile.removeEventListener("click", tileListener)
+        })
     }
 
-    return { renderBoard , addTileListeners };
+    const addTileListeners = () => {
+        tiles.forEach(tile => {
+            tile.addEventListener("click", tileListener);
+        });
+    };
+
+    return { renderBoard , addTileListeners , removeTileListeners};
 })();
